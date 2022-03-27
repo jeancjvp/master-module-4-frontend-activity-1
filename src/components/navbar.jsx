3ea19d82fc87e9e82aac3dd4fb7843ec41e841b5
Jean Carlos Villarroel <87240193+jeancjvp@users.noreply.github.com>
2022-03-27 13:56:01 -0500

    adding mouse pointer for desktop

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,11 +2,11 @@ function NavBar({onLogoClick, onProfileClick}) {
     return (
         <nav className="navbar navbar-light bg-light px-3">
             <div className="container-fluid">
-                <div onClick={ () => { onLogoClick() } }>
+                <div className="btn" onClick={ () => { onLogoClick() } }>
                     <i className="fa-solid fa-globe"></i>
                     <span className="navbar-brand mb-0 h1 ms-2">Worldfile</span>
                 </div>
-                <div onClick={ () => { onProfileClick() } }>
+                <div className="btn" onClick={ () => { onProfileClick() } }>
                     <i className="fa-solid fa-user"></i>
                 </div>
             </div>
